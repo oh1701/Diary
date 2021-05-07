c7ce585fa37c6c697c0f6a04334b4cf3e6aee88b
oh1701 <rbtjd2290@naver.com>
2021-05-07 22:09:01 +0900

    0507_2208

--- app/src/main/java/com/diary/diary/Shortcuts.kt
+++ app/src/main/java/com/diary/diary/Shortcuts.kt
@@ -2,10 +2,25 @@ package com.diary.diary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.diary.diary.databinding.ActivityShortcutsBinding
 
+class ShortcutsViewModel:ViewModel(){
+
+}
 class Shortcuts : AppCompatActivity() {
+
+    lateinit var binding:ActivityShortcutsBinding
+    lateinit var viewModel:ShortcutsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shortcuts)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_shortcuts)
+        viewModel = ViewModelProvider(this).get(ShortcutsViewModel::class.java)
+        binding.lifecycleOwner = this
+        binding.shortcuts = viewModel
     }
 }
